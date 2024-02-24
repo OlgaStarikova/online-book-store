614bafcde5ccce1d1b8eeb5bb8fa9313fad080ac
Olga Starkova <ole4kastar79@gmail.com>
2024-02-24 20:10:13 +0200

    created bookService online-book-store

--- src/main/java/com/example/onlinebookstore/OnlineBookStoreApplication.java
+++ src/main/java/com/example/onlinebookstore/OnlineBookStoreApplication.java
@@ -1,13 +1,34 @@
 package com.example.onlinebookstore;
 
+import com.example.onlinebookstore.model.Book;
+import com.example.onlinebookstore.service.BookService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.math.BigDecimal;
 
 @SpringBootApplication
 public class OnlineBookStoreApplication {
+    @Autowired
+    private BookService bookService;
 
     public static void main(String[] args) {
         SpringApplication.run(OnlineBookStoreApplication.class, args);
     }
 
+    @Bean
+    public CommandLineRunner commandLineRunner() {
+        return args -> {
+            Book bookTomSoyer = new Book();
+            bookTomSoyer.setTitle("Tom Soyer");
+            bookTomSoyer.setAuthor("Mark Twen");
+            bookTomSoyer.setIsbn("111");
+            bookTomSoyer.setPrice(BigDecimal.valueOf(120.00));
+            Book bookSaved = bookService.save(bookTomSoyer);
+            System.out.println(bookService.findAll());
+        };
+    }
 }
