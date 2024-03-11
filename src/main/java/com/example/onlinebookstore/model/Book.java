446cbd7da7affdb7b0217ca4b8cb3ec82b3a99a2
Olga Starikova <ole4kastar79@gmail.com>
2024-03-11 23:05:49 +0200

    liquibase,soft delete,update

--- src/main/java/com/example/onlinebookstore/model/Book.java
+++ src/main/java/com/example/onlinebookstore/model/Book.java
@@ -1,14 +1,19 @@
 package com.example.onlinebookstore.model;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import java.math.BigDecimal;
 import lombok.Data;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.SQLRestriction;
 
 @Entity
 @Data
+@SQLDelete(sql = "UPDATE book SET is_deleted = true WHERE id = ?")
+@SQLRestriction("is_deleted = false")
 public class Book {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -19,4 +24,6 @@ public class Book {
     private BigDecimal price;
     private String description;
     private String coverImage;
+    @Column(nullable = false)
+    private boolean isDeleted = false;
 }
