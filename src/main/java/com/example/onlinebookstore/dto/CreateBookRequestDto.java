6f2d236ff12e34f700724c0eed543b1d5258cf87
Olga Starikova <ole4kastar79@gmail.com>
2024-03-27 01:41:52 +0200

    @NotBlank enough

--- src/main/java/com/example/onlinebookstore/dto/CreateBookRequestDto.java
+++ src/main/java/com/example/onlinebookstore/dto/CreateBookRequestDto.java
@@ -3,22 +3,14 @@ package com.example.onlinebookstore.dto;
 import com.example.onlinebookstore.validation.Isbn;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import java.math.BigDecimal;
 
 public class CreateBookRequestDto {
-    @NotNull
     @NotBlank
-    @NotEmpty
     private String title;
-    @NotNull
     @NotBlank
-    @NotEmpty
     private String author;
-    @NotNull
-    @NotBlank
-    @NotEmpty
     @Isbn
     private String isbn;
     @NotNull
