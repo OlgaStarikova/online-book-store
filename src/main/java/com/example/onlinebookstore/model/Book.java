5f3693165fe983f0845652629b30386cc3e31847
Olga Starikova <ole4kastar79@gmail.com>
2024-05-06 00:07:30 +0300

    improoved version according mentor's advice 4

--- src/main/java/com/example/onlinebookstore/model/Book.java
+++ src/main/java/com/example/onlinebookstore/model/Book.java
@@ -7,10 +7,14 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import java.math.BigDecimal;
+import lombok.Getter;
+import lombok.Setter;
 import org.hibernate.annotations.SQLDelete;
 import org.hibernate.annotations.SQLRestriction;
 
 @Entity
+@Getter
+@Setter
 @SQLDelete(sql = "UPDATE books SET is_deleted = true WHERE id = ?")
 @SQLRestriction("is_deleted = false")
 @Table(name = "books")
@@ -22,7 +26,7 @@ public class Book {
     private String title;
     @Column(nullable = false)
     private String author;
-    @Column(unique = true)
+    @Column(unique = true, nullable = false)
     private String isbn;
     @Column(nullable = false)
     private BigDecimal price;
@@ -30,68 +34,4 @@ public class Book {
     private String coverImage;
     @Column(nullable = false)
     private boolean isDeleted = false;
-
-    public Long getId() {
-        return this.id;
-    }
-
-    public String getTitle() {
-        return this.title;
-    }
-
-    public String getAuthor() {
-        return this.author;
-    }
-
-    public String getIsbn() {
-        return this.isbn;
-    }
-
-    public BigDecimal getPrice() {
-        return this.price;
-    }
-
-    public String getDescription() {
-        return this.description;
-    }
-
-    public String getCoverImage() {
-        return this.coverImage;
-    }
-
-    public boolean isDeleted() {
-        return this.isDeleted;
-    }
-
-    public void setId(final Long id) {
-        this.id = id;
-    }
-
-    public void setTitle(final String title) {
-        this.title = title;
-    }
-
-    public void setAuthor(final String author) {
-        this.author = author;
-    }
-
-    public void setIsbn(final String isbn) {
-        this.isbn = isbn;
-    }
-
-    public void setPrice(final BigDecimal price) {
-        this.price = price;
-    }
-
-    public void setDescription(final String description) {
-        this.description = description;
-    }
-
-    public void setCoverImage(final String coverImage) {
-        this.coverImage = coverImage;
-    }
-
-    public void setDeleted(final boolean isDeleted) {
-        this.isDeleted = isDeleted;
-    }
 }
--- src/main/java/com/example/onlinebookstore/model/User.java
+++ src/main/java/com/example/onlinebookstore/model/User.java
@@ -6,8 +6,16 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.SQLRestriction;
 
 @Entity
+@Getter
+@Setter
+@SQLDelete(sql = "UPDATE users SET is_deleted = true WHERE id = ?")
+@SQLRestriction("is_deleted = false")
 @Table(name = "users")
 public class User {
     @Id
@@ -22,6 +30,8 @@ public class User {
     @Column(nullable = false)
     private String lastName;
     private String shippingAddress;
+    @Column(nullable = false)
+    private boolean isDeleted = false;
 
     public Long getId() {
         return this.id;
