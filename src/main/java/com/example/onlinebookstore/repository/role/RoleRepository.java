2001931aaf7cf7fb475420d993d7ff1c0c7ee1df
Olga Starikova <ole4kastar79@gmail.com>
2024-05-13 16:38:50 +0300

    spring boot security, roles, PreAuthorize access

---
+++ src/main/java/com/example/onlinebookstore/repository/role/RoleRepository.java
@@ -0,0 +1,9 @@
+package com.example.onlinebookstore.repository.role;
+
+import com.example.onlinebookstore.model.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Long> {
+}
--- src/main/java/com/example/onlinebookstore/service/impl/UserServiceImpl.java
+++ src/main/java/com/example/onlinebookstore/service/impl/UserServiceImpl.java
@@ -4,9 +4,11 @@ import com.example.onlinebookstore.dto.UserRegistrationRequestDto;
 import com.example.onlinebookstore.dto.UserResponseDto;
 import com.example.onlinebookstore.exception.RegistrationException;
 import com.example.onlinebookstore.mapper.UserMapper;
+import com.example.onlinebookstore.model.User;
 import com.example.onlinebookstore.repository.user.UserRepository;
 import com.example.onlinebookstore.service.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @RequiredArgsConstructor
@@ -14,6 +16,7 @@ import org.springframework.stereotype.Service;
 public class UserServiceImpl implements UserService {
     private final UserRepository userRepository;
     private final UserMapper userMapper;
+    private final PasswordEncoder passwordEncoder;
 
     @Override
     public UserResponseDto register(UserRegistrationRequestDto requestDto) {
@@ -22,6 +25,12 @@ public class UserServiceImpl implements UserService {
                     + requestDto.email()
                     + " is already registered");
         }
-        return userMapper.toDto(userRepository.save(userMapper.toModel(requestDto)));
+        User user = new User();
+        user.setPassword(passwordEncoder.encode(requestDto.password()));
+        user.setEmail(requestDto.email());
+        user.setFirstName(requestDto.firstName());
+        user.setLastName(requestDto.lastName());
+        user.setShippingAddress(requestDto.shippingAddress());
+        return userMapper.toDto(userRepository.save(user));
     }
 }
