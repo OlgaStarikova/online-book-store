79237d354463222d05b71e4292057ee23dccccea
Olga Starikova <ole4kastar79@gmail.com>
2024-05-21 00:01:23 +0300

    Login Authentication with JwtFilter

--- src/main/java/com/example/onlinebookstore/controller/AuthenticationController.java
+++ src/main/java/com/example/onlinebookstore/controller/AuthenticationController.java
@@ -1,8 +1,11 @@
 package com.example.onlinebookstore.controller;
 
+import com.example.onlinebookstore.dto.UserLoginRequestDto;
+import com.example.onlinebookstore.dto.UserLoginResponseDto;
 import com.example.onlinebookstore.dto.UserRegistrationRequestDto;
 import com.example.onlinebookstore.dto.UserResponseDto;
 import com.example.onlinebookstore.exception.RegistrationException;
+import com.example.onlinebookstore.security.AuthenticationService;
 import com.example.onlinebookstore.service.UserService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -19,6 +22,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/auth")
 public class AuthenticationController {
     private final UserService userService;
+    private final AuthenticationService authenticationService;
 
     @Operation(summary = "Register a new user", description = "Registration "
             + " a user for a access to data")
@@ -26,5 +30,13 @@ public class AuthenticationController {
     public UserResponseDto register(@RequestBody @Valid UserRegistrationRequestDto request)
             throws RegistrationException {
         return userService.register(request);
+    }
+
+    @Operation(summary = "Login a user", description = "Authentication and login "
+            + " a user for a access to data")
+    @PostMapping("/login")
+    public UserLoginResponseDto login(@RequestBody @Valid UserLoginRequestDto requestDto) {
+        String string = "w";
+        return authenticationService.authenticate(requestDto);
     }
 }
