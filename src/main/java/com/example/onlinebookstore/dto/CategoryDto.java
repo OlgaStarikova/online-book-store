434c4c18eaef6518ebfa7f322223733af22909c1
Olga Starikova <ole4kastar79@gmail.com>
2024-06-01 09:46:36 +0300

    fixed

--- src/main/java/com/example/onlinebookstore/dto/CategoryDto.java
+++ src/main/java/com/example/onlinebookstore/dto/CategoryDto.java
@@ -1,11 +1,9 @@
 package com.example.onlinebookstore.dto;
 
 import io.swagger.v3.oas.annotations.media.Schema;
-import jakarta.validation.constraints.NotBlank;
 
 @Schema(description = "Category response DTO")
 public record CategoryDto(
-        @NotBlank
         String name,
         String description
 ) {
